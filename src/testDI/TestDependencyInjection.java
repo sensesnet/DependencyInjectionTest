c7a7478fd24314f73719f102e88ae6ca51835109
Kirill Degtyarev <sensesnet@gmail.com>
2017-04-29 10:59:17 +0300

    Dependency injection

--- src/testDI/TestDependencyInjection.java
+++ src/testDI/TestDependencyInjection.java
@@ -13,10 +13,10 @@ public class TestDependencyInjection {
 
     public static void main(String[] args) {
 
-        Address_ENG address_eng = new Address_ENG("Tomas_Edison st.", 12);
-        Address_RUS address_rus = new Address_RUS("Belarus", "7", "Minsk", "FreeBelarus", 12);
-        Person person_eng = new Person(1, "Kirill", "Degtyarev", 20, (IAddress) address_eng);
-        Person person_rus = new Person(2, "Irina", "Degtyareva", 18, (IAddress) address_rus);
+        IAddress address_eng = new Address_ENG("Tomas_Edison st.", 12);
+        IAddress address_rus = new Address_RUS("Belarus", "7", "Minsk", "FreeBelarus", 12);
+        Person person_eng = new Person(1, "Kirill", "Degtyarev", 20, address_eng);
+        Person person_rus = new Person(2, "Irina", "Degtyareva", 18, address_rus);
 
         Address_ENG address_eng_by_person = (Address_ENG) person_eng.getAddress();
         Address_RUS address_rus_by_person = (Address_RUS) person_rus.getAddress();
@@ -24,7 +24,8 @@ public class TestDependencyInjection {
         System.out.println("English person: "
                 + person_eng.getFirstName() + " "
                 + person_eng.getSecondName() + " address: "
-                + person_eng.getAddress() + " "
+                + ((Address_ENG) person_eng.getAddress()).getStreet() + " "
+                + ((Address_ENG) person_eng.getAddress()).getNumberOfHome() + " | "
                 + address_eng_by_person.getStreet() + " "
                 + address_eng_by_person.getNumberOfHome()
         );
@@ -32,7 +33,8 @@ public class TestDependencyInjection {
         System.out.println("Russian person: "
                 + person_rus.getFirstName() + " "
                 + person_rus.getSecondName() + " address: "
-                + person_rus.getAddress() + " "
+                + ((Address_RUS) person_rus.getAddress()).getStreet() + " "
+                + ((Address_RUS) person_rus.getAddress()).getNumberOfFlat() + " | "
                 + address_rus_by_person.getStreet() + " "
                 + address_rus_by_person.getNumberOfFlat()
         );
